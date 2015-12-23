fd2a0a4c6ccd491699e614c50995b474b4041a5b
chris_p_maurer <chris_p_maurer@yahoo.com>
2015-12-23 07:40:33 -0600

    updated sketch, changed AT commands to use AT+HTTP

--- sketch_feb28a/sketch_feb28a.ino
+++ sketch_feb28a/sketch_feb28a.ino
@@ -1,11 +1,14 @@
-/*  
+#include <EEPROM.h>
+
+/*
   GPRS
   Temperature
   Moisture
 
   Temperature: Pin 10
   Moisture: Pin 0
-  GPRS: Pins 7 and 8
+  GPRS: Pins 18 and 19
+  Photo Cell: Pin 8
 
 */
 #include <OneWire.h>
@@ -15,29 +18,29 @@
 
 #define ONEWIRE 10 //pin to use for One Wire interface
 #define MOISTURE 0 //pin to use for Moisture Sensor
-#define DELAY 5000
+#define PHOTO 8 //pin to use for Photocell Sensor
+#define AMBIENT 9 //pin to use for Ambient Light Sensor
+
+#define DELAY 60000 //delay each measurement for 60 seconds
+
+#define SEMATEXT_TOKEN String("token")
 
 OneWire oneWire(ONEWIRE);
 DallasTemperature sensors(&oneWire);
 
-//-------------------------------------------------------------
-//---------------------Ubidots Configuration-------------------
-//-------------------------------------------------------------
-String token = "";      
-String idvariable = "";                                  
-
 void setup() {
   //the GPRS baud rate
-  Serial1.begin(19200);                                                             
-  Serial.begin(9600);
+  Serial1.begin(19200);
+  Serial.begin(19200);
+
   // Start up the OneWire Sensors library
   sensors.begin();
   delay(1000);
-  Serial.println(ONEWIRE);
+
 }
 
 void loop() {
-  String var;
+
   //Temperature
   sensors.requestTemperatures(); // Send the command to get temperatures
   float tempC = sensors.getTempCByIndex(0);
@@ -45,102 +48,89 @@ void loop() {
   Serial.print("Fahrenheit: ");
   Serial.print(tempF);
   Serial.println(" F ..........DONE");
-  
+
   //Moisture
   int moistureValue = analogRead(MOISTURE);
   Serial.print("moistureValue: ");
   Serial.println(moistureValue);
+
+  //Moisture Voltage
   float moistureVoltage = moistureValue * (5.0 / 1023.0);
   Serial.print("voltage: ");
   Serial.println(moistureVoltage);
-  
-  var = "{\"fahrenheit\":"+ String(tempF) + ",\"moisture\":" + String(moistureValue) + ",\"voltage\":" + String(moistureVoltage) + "}";  
-  Serial.println(var);
- // upload(var);
 
-  //Ubidots
+  int lightLevel = analogRead(PHOTO);
+  Serial.print("light: ");
+  Serial.println(lightLevel);
+
+  int ambientLightLevel = analogRead(AMBIENT);
+  Serial.print("ambient light: ");
+  Serial.println(ambientLightLevel);
+
+  //send the data
+  upload(tempC, tempF, moistureValue, moistureVoltage, lightLevel, ambientLightLevel);
+
   delay(DELAY);
 }
 
-void upload(String value){
-  int num;
-  String le;
-  String var;
-  var="{\"value\":"+ value + "}";
-  num=var.length();
-  le=String(num);
-  for(int i = 0; i < 7; i++){    
-    //this is made repeatedly because it is unstable    
-    Serial1.println("AT+CGATT?");
-    delay(2000);
-    ShowSerialData();
-  }
-  
-  //replace with your providers' APN
-  Serial1.println("AT+CSTT=\"att.mvno\"");                                    
-  delay(1000);
+void upload(float tempc, float tempf, int moistureValue, float moistureVoltage, int lightLevel, int ambientLightLevel) {
+
+    String payload = convertToJsonString(tempc, tempf, moistureValue, moistureVoltage, lightLevel, ambientLightLevel);
+    int num = payload.length();
+
+    sendATCommand("AT+CIPCLOSE", 100);
+    sendATCommand("AT+CIPSHUT", 100);
+
+    sendATCommand("AT+CPIN?", 1000);
+    sendATCommand("AT+CSQ", 1000);
+    sendATCommand("AT+CREG?", 1000);
+    sendATCommand("AT+CGATT?", 500);
+
+    sendATCommand("AT+CGATT=1", 500);
+
+    sendATCommand("AT+SAPBR=3,1,\"CONTYPE\",\"GPRS\"", 500);
+    sendATCommand("AT+SAPBR=3,1,\"APN\",\"wholesale\"", 500);
+    sendATCommand("AT+SAPBR=1,1", 3000);
+
+    sendATCommand("AT+HTTPINIT", 3000);
+    sendATCommand("AT+HTTPPARA=\"CID\",1", 500);
+    sendATCommand("AT+HTTPPARA=\"URL\",\"logsene-receiver.sematext.com/"+SEMATEXT_TOKEN+"/field-telemetry\"", 500);
+    sendATCommand("AT+HTTPPARA=\"CONTENT\",\"application/json\"", 500);
+    sendATCommand("AT+HTTPPARA=\"HOST\",\"https://logsene-receiver.sematext.com\"", 500);
+
+    sendATCommand("AT+HTTPDATA=" + String(payload.length()) + ",5000", 500);
+    writeData(payload, 1000);
+    sendATCommand("AT+HTTPACTION=1", 5000);
+    sendATCommand("AT+HTTPREAD", 500);
+    sendATCommand("AT+HTTPTERM", 500);
+    sendATCommand("AT+SAPBR=0,1", 500);
+
+}
+
+String convertToJsonString(float tempc, float tempf, int moistureValue, float moistureVoltage, int lightLevel, int ambientLightLevel) {
+ return "{ \"temperature_celcius\": " +  String(tempc) + ", \"temperature_fahrenheit\": " + String(tempf) + ", \"moisture_value\":" + String(moistureValue) + ",\"moisture_voltage\": " + String(moistureVoltage) + ", \"light_level\":" + String(lightLevel)+ ",\"ambient_light_level\":" + String(ambientLightLevel) + " }";
+}
+
+void writeData(String data, int _delay) {
+  Serial1.print(data);
+  delay(_delay);
   ShowSerialData();
-  //bring up wireless connection
-  Serial1.println("AT+CIICR");
-  delay(3000);
+}
+
+void writeData(char data, int _delay) {
+  Serial1.print(data);
+  delay(_delay);
   ShowSerialData();
-  //get local IP adress
-  Serial1.println("AT+CIFSR");
-  delay(2000);
-  ShowSerialData();
-  Serial1.println("AT+CIPSPRT=0");
-  delay(3000);
-  ShowSerialData();
-  //start up the connection
-  Serial1.println("AT+CIPSTART=\"tcp\",\"things.ubidots.com\",\"80\"");
-  delay(3000);
-  ShowSerialData();
-  //begin send data to remote server
-  Serial1.println("AT+CIPSEND");
-  delay(3000);
-  ShowSerialData();
-  Serial1.print("POST /api/v1.6/variables/" + idvariable);
-  delay(100);
-  ShowSerialData();
-  Serial1.println("/values HTTP/1.1");
-  delay(100);
-  ShowSerialData();
-  Serial1.println("Content-Type: application/json");
-  delay(100);
-  ShowSerialData();
-  Serial1.println("Content-Length: "+le);
-  delay(100);
-  ShowSerialData();
-  Serial1.print("X-Auth-Token: ");
-  delay(100);
-  ShowSerialData();
-  Serial1.println(token);
-  delay(100);
-  ShowSerialData();
-  Serial1.println("Host: things.ubidots.com");
-  delay(100);
-  ShowSerialData();
-  Serial1.println();
-  delay(100);
-  ShowSerialData();
-  Serial1.println(var);
-  delay(100);
-  ShowSerialData();
-  Serial1.println();
-  delay(100);
-  ShowSerialData();
-  Serial1.println((char)26);
-  delay(7000);
-  Serial1.println();
-  ShowSerialData();
-  //close the communication
-  Serial1.println("AT+CIPCLOSE");
-  delay(1000);
+}
+
+void sendATCommand(String data, int _delay) {
+  Serial1.println(data);
+  delay(_delay);
   ShowSerialData();
 }
 
 void ShowSerialData() {
-  while(Serial1.available()!=0)
-  Serial.write(Serial1.read());
+  while(Serial1.available() != 0){
+   Serial.write(Serial1.read());
+  }
 }
-
